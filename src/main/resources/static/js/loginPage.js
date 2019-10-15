c6c34b2ea76215c6ee6eedb067208e851c31ad38
Anirudh <anirudhkulkarni1997@gmail.com>
2019-10-15 11:13:13 +0530

    Changed password requirement. (minor)

--- src/main/resources/static/js/loginPage.js
+++ src/main/resources/static/js/loginPage.js
@@ -3,12 +3,12 @@ let email = document.getElementById("email");
 let password = document.getElementById("password");
 const valid = function () {
     const reg1 = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,6})+$");
-    const reg2 = new RegExp("^([1-zA-Z0-1@.]{8,20})$");
+    const reg2 = new RegExp("^([1-zA-Z0-1@.]{6,20})$");
     if (reg1.test(email.value)) {
         if (reg2.test(password.value)) {
             return true;
         } else {
-            message.innerText = "Password must be eight characters long\n Password can only contain '0-9,a-z,A-Z,_,.,@'";
+            message.innerText = "Password must be six characters long\n Password can only contain '0-9,a-z,A-Z,_,.,@'";
         }
     } else {
         message.innerText = "Bad email";
