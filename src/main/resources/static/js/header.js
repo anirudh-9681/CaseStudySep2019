7e0ceb455fe50d11d7f9df5f2c23ccfab1f1f0cf
Anirudh <anirudhkulkarni1997@gmail.com>
2019-10-18 12:01:56 +0530

    Changed search page as all categories filter was not working

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -28,7 +28,7 @@ function addToCategoryList(cats) {
     }
     const copy = catTemplate.cloneNode(true).content;
     copy.children[0].children[0].innerText = "All";
-    copy.children[0].children[0].href = `/search?category=All`;
+    copy.children[0].children[0].href = `/search?category=all`;
     catContainer.insertChildAtIndex(copy, 0);
 }
 
